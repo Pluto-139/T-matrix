be66cd51f92653eb576d90c2632dba25eab78b41
Pluto-139 <138948051+Pluto-139@users.noreply.github.com>
2026-02-23 10:21:53 +0100

    Update resource allocation and test script in Fermi.sh

--- test/test_Fermi.sh
+++ test/test_Fermi.sh
@@ -13,9 +13,9 @@
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=56
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=2G
+#SBATCH --mem=56G
 
 
 
@@ -42,7 +42,7 @@ echo "Allocated CPUs: $SLURM_CPUS_PER_TASK"
 cd /home/PERSONALE/jinghao.wang2/code/test
 
 
-python test_Fermi.py
+python mutest.py
 # python test_mu.py
 # python test_interaction.py
 
